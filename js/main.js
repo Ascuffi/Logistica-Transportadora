8d176c82325e42dfe2aaf43adf662c4a35999a8f
Ascuffi <Scuffiaxel96@gmail.com>
2026-08-22 09:15:36 -0300

    Add scroll event to change header class

--- js/main.js
+++ js/main.js
@@ -127,3 +127,14 @@ document.addEventListener('DOMContentLoaded', function(){
 
   // Small enhancement: if hero video absent, show poster (browser handles it) and ensure contrast
 });
+const header = document.querySelector(".site-header");
+
+window.addEventListener("scroll", () => {
+
+    if(window.scrollY > 100){
+        header.classList.add("scrolled");
+    } else {
+        header.classList.remove("scrolled");
+    }
+
+});
